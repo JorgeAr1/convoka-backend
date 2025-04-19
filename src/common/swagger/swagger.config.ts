57e7630dc3f2de6f7cc5892d8c6338ee901e9f23
Jorge Luis Arreygue <coke_1224@hotmail.com>
2025-04-18 18:55:20 -0600

    update swagger description for the project

--- src/common/swagger/swagger.config.ts
+++ src/common/swagger/swagger.config.ts
@@ -1,34 +1,34 @@
 
 const customfavIcon = "https://res.cloudinary.com/dphleqb5t/image/upload/v1713724442/jc-develop/favicon-c_qlvrpv.png";      //adding our favicon to swagger
 
-const customCss = `
-    .topbar{
-        animation: navanimation linear both;
-        animation-range: 0 300px;
-        animation-timeline: scroll(root);
-        position: sticky;
-        top: 0px;
-        z-index: 1
-    }
-    .topbar-wrapper {
-        content: Prueba; color: white;
-    }  
-    .topbar-wrapper a {
-        content:url(https://res.cloudinary.com/dphleqb5t/image/upload/v1713730346/rest-api-template/Logo-Swagger_ukcytn.png); width:200px; height:auto;
-    }
-    .swagger-ui .opblock .opblock-summary-description { 
-        font-weight: 900 
-    }
-    .description .renderedMarkdown p {
-        font-size: 1rem;
-    }
-    @keyframes navanimation {
-        to {
-            opacity: 0.9;
-            backdrop-filter: blur(10px);
-        }
-    }
-`
+// const customCss = `
+//     .topbar{
+//         animation: navanimation linear both;
+//         animation-range: 0 300px;
+//         animation-timeline: scroll(root);
+//         position: sticky;
+//         top: 0px;
+//         z-index: 1
+//     }
+//     .topbar-wrapper {
+//         content: Prueba; color: white;
+//     }  
+//     .topbar-wrapper a {
+//         content:url(https://res.cloudinary.com/dphleqb5t/image/upload/v1713730346/rest-api-template/Logo-Swagger_ukcytn.png); width:200px; height:auto;
+//     }
+//     .swagger-ui .opblock .opblock-summary-description { 
+//         font-weight: 900 
+//     }
+//     .description .renderedMarkdown p {
+//         font-size: 1rem;
+//     }
+//     @keyframes navanimation {
+//         to {
+//             opacity: 0.9;
+//             backdrop-filter: blur(10px);
+//         }
+//     }
+// `
 
 const customSiteTitle = "{jc-develop Auth API-REST}";   //add site title to swagger for nice SEO
 
@@ -38,7 +38,7 @@ const customJsStr = "alert('prueba')";          //uncomment this line to add a c
 
 const swaggerOptions = {
     customfavIcon,
-    customCss,
+    // customCss,
     customSiteTitle,
     // customJs,   //uncomment this line to add a custom script file
     // customJsStr,  //uncomment this line to add a custom script
@@ -49,18 +49,22 @@ const swaggerOptions = {
     },
 }
 
-const swaggerTitle = "Nest Postgres Authentication REST API Template Documentation"
+const swaggerTitle = "Convoka App REST API Documentation"
 
 const swaggerDescription = `
-  <p>This Nest.js REST API template provides a robust foundation for building secure and scalable web applications. With built-in user registration, JWT authentication, and protected routes.</p>
-  <p>The API follows RESTful principles, making it easy to integrate and interact with from various client applications.</p>
-  <p>Key features of this API template include:</p>
-  <ul>
-    <li>User Registration and Management</li>
-    <li>JWT Token-based Authentication</li>
-    <li>Role-based Access Control for Protected Routes</li>
-    <li>CRUD Operations for User Entities</li>
-  </ul>
+Convoka es una plataforma diseñada para iglesias y ministerios cristianos que desean organizar, gestionar y registrar eventos de forma eficiente.
+
+Esta API permite:
+- Crear y administrar **organizaciones** (iglesias o ministerios)
+- Registrar y controlar **usuarios** con distintos roles y permisos
+- Gestionar **eventos** con asistentes, costos, pagos y roles personalizados
+- Facilitar el **check-in**, el registro por QR y la recopilación de datos en tiempo real
+- Soportar relaciones familiares entre asistentes (como matrimonios o tutores)
+- Brindar una experiencia multiusuario y multiubicación según el plan contratado
+
+Esta API está protegida con autenticación basada en JWT, y su uso requiere tener un token válido para acceder a la mayoría de los endpoints.
+
+Todos los planes incluyen funcionalidades básicas y escalables. Convoka es una herramienta pensada para servir al Reino, facilitando la organización y convocación de personas para propósitos con impacto eterno.
 `
 
 export {
